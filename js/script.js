6db1f032fc4c512ece9b8e74ddfbfa8709a80392
Tomme-git <tommehansen@gmail.com>
2022-09-19 12:07:36 +0200

    lmao

--- js/script.js
+++ js/script.js
@@ -12,8 +12,14 @@ let greeting;
 //Time for today
 let time = hourNow + ":" + minutesNow;
 
+// Current day of the month (local)
+let dateNow = today.getDate();
+
+// Current year (local)
+let yearNow = today.getFullYear();
+
 //Weekday array
-let weekday = new Array(7);
+const weekday = new Array(7);
 weekday[0] = "Sunday";
 weekday[1] = "Monday";
 weekday[2] = "Tuesday";
@@ -22,6 +28,10 @@ weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
 
+const month = ["1","2","3","4","5","6","7","8","9","10","11","12"];
+
+// Current month of the year (local)
+let monthNow = month[today.getMonth()];
 
 //Use conditions to find out what greeting to use. Morning = 5 to 12, Afternoon = 13 to 17, Evening = 17 to 4. Use else if because of more than 2 conditions.
 if (hourNow >= 5 && hourNow <= 12){
@@ -40,7 +50,19 @@ greeting = "Good Evening";
 document.write("<h3>" + greeting + "</h3>");
 
 //time now
-document.write("<h3>" + time + "</h3>")
+document.write("<h3>" + time + "</h3>");
 
 //today.getDay() gives a number for current day. weekday[] prints out the day from the number received
-document.write("<h4>" + weekday[today.getDay()] + "</h4>")
+document.write("<h4>" + weekday[today.getDay()] + "</h4>");
+
+
+
+
+function writeDate() {
+    document.getElementById("currentdate").innerHTML = "<h4>" + dateNow + "/" + monthNow + "/" + yearNow + "</h4>";
+}
+
+function myFunction(){
+    document.getElementById("demo").innerHTML = "lolmao";
+    writeDate();
+}
